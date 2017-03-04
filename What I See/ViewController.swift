9ade7f1eb3d96c0007a2281fde8fd86e8c9a0189
Ibrahim Alkhatib <ibrahim_speedfire95@icloud.com>
2017-03-04 14:47:51 +0200

    asasdasdasd

--- What I See/ViewController.swift
+++ What I See/ViewController.swift
@@ -17,55 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        let a = UIButton(frame: CGRect(x: 50, y: 50, width: 150, height: 50))
-        a.setTitle("add group", for: .normal)
-        a.setTitleColor(UIColor.purple, for: .normal)
-        a.addTarget(self, action: #selector(createGroup), for: .touchUpInside)
-        self.view.addSubview(a)
-        
-        
-        
-        
-        let b = UIButton(frame: CGRect(x: 50, y: 250, width: 150, height: 50))
-        b.setTitle("add group", for: .normal)
-        b.setTitleColor(UIColor.purple, for: .normal)
-        b.addTarget(self, action: #selector(createGroup), for: .touchUpInside)
-        self.view.addSubview(b)
-
-        
-        
-        
-        let c = UIButton(frame: CGRect(x: 50, y: 450, width: 150, height: 50))
-        c.setTitle("add group", for: .normal)
-        c.setTitleColor(UIColor.purple, for: .normal)
-        c.addTarget(self, action: #selector(createGroup), for: .touchUpInside)
-        self.view.addSubview(c)
-
     }
-
-    func createGroup() {
-        let client = MPOFaceServiceClient(subscriptionKey: MicrosoftFaceAPISubscriptionKey)
-
-        let x = client?.createPersonGroup(withId: "asd", name: "friends", userData: nil, completionBlock: { (error) in
-            if error == nil {
-                let group = MPOPersonGroup()
-                group.personGroupId = "asd"
-                group.name = "friends"
-                self.groups.append(group)
-            }
-        })
-        x?.resume()
-    }
-    
-    
-    func addPersonToGroup() {
-        let client = MPOFaceServiceClient(subscriptionKey: MicrosoftFaceAPISubscriptionKey)
-
-        let x = client?.addPersonFace(withPersonGroupId: "", personId: <#T##String!#>, data: <#T##Data!#>, userData: <#T##String!#>, faceRectangle: <#T##MPOFaceRectangle!#>, completionBlock: <#T##((MPOAddPersistedFaceResult?, Error?) -> Void)!##((MPOAddPersistedFaceResult?, Error?) -> Void)!##(MPOAddPersistedFaceResult?, Error?) -> Void#>)
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
